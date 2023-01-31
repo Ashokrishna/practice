fa437d4e81a946bcd26682ca1cbb9b0477785eb5
Ashokrishna <93512764+Ashokrishna@users.noreply.github.com>
2023-01-31 22:35:43 +0530

    Add files via upload

---
+++ 31-01-2023/program/Student.java
@@ -0,0 +1,52 @@
+class Student
+{
+	public static void main (String[] args){
+		int id=50;
+		int mark_A = 80;
+		int mark_B = 98;
+		int mark_C = 81;
+		int mark_D = 76;
+		int mark_E = 54;
+		
+		System.out.println(id);
+		System.out.println(id);
+		System.out.println(id);
+		System.out.println(id);
+		System.out.println(id);
+
+		//Integer
+		System.out.println("Integer:");
+		System.out.println(10+10); 
+		System.out.println(10+10*2/3);
+		System.out.println(6*3/8+2-11);
+		
+		//String
+		System.out.println("String :");
+		System.out.println("10+10/2");
+		System.out.println("Hello");
+		System.out.println("Done");
+		
+		//Boolean
+		System.out.println("Boolean :");
+		System.out.println(false); 
+		System.out.println(true);
+		
+		//Fload
+		System.out.println("Float :");
+		System.out.println(30.35f);
+		System.out.println(4/5.0+2*100);
+		
+		//long
+		System.out.println(30.35);
+		
+
+	
+
+		System.out.println(1 +".Mark of A : " + mark_A);
+		System.out.println(2 +".Mark of B: " + mark_B);
+		System.out.println(3 +".Mark of C: " + mark_C);
+		System.out.println(4 +".Mark of D: " + mark_D);
+		System.out.println(5 +".Mark of E: " + mark_E);
+		System.out.println("My Total marks of all: " + (mark_A + mark_B + mark_C + mark_D + mark_E));
+	}
+}
